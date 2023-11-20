7f22e37d038d2a3cea27b6b101614ddc066c65ed
anajuliarauber <anajuliarauber@hotmail.com>
2023-11-20 17:12:57 -0300

    docs: add swagger config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
@@ -13,5 +15,8 @@ app.use((error, req, res, next) => {
     .status(error.code ?? 500)
     .json({ error: error.message ?? "Something went wrong" });
 });
+
+app.use("/api-docs", swaggerUi.serve);
+app.use("/api-docs", swaggerUi.setup(swaggerFile));
 
 export { app };
